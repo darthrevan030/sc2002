9091f91080677a82016541127739fa92861316ff
darth.revan.030 <smartsamarth03@gmail.com>
2025-09-11 10:57:34 +0800

    Lab 2 P 3 Not Done

---
+++ Labs/Lab 2/Lab2p3.java
@@ -0,0 +1,16 @@
+/*
+Write the method divide() which does division by subtraction and returns the quotient of dividing m by
+n. Both m and n are positive integers (i.e. 1,23,4,…). Division by subtraction means that the division
+operation is achieved using the subtraction method. For example, divide(12,4) will be performed as
+follows: 12-4=8, 8-4=4, and then 4-4=0, and it ends and returns the result of 3 as it performs three
+times in the subtraction operation. No error checking on the parameters is required in the method. 
+ */
+
+import java.util.Scanner
+
+public class Lab2p3{
+
+    public static void divide(int m, int n){
+
+    }
+}
