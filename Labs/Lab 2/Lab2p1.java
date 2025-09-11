fcde0c0c1e533d72b8d1a6839fa56cc02b7d2581
darth.revan.030 <smartsamarth03@gmail.com>
2025-09-11 11:05:33 +0800

    Move Lab 2 to just 1 file

--- Labs/Lab 2/Lab2p1.java
+++ Labs/Lab 2/Lab2p1.java
@@ -17,6 +17,7 @@ public class Lab2p1 {
             
             switch (choice) {
                 case 1: /* add mulTest() call */
+
                     break;
                 case 2: /* add divide() call */
                     break;
@@ -33,6 +34,66 @@ public class Lab2p1 {
         } while (choice < 7);
     }
 
-        /* add method code here */
+    public static void mulTest(String[] args) {
+        /*
+        Write a method that is to test students ability to do multiplication. The method will ask a student 5
+        multiplication questions one by one and checks the answers. The method prints out the number of
+        correct answers given by the student. The method random() from the Math class of the Java library
+        can be used to produce two positive one-digit integers (i.e. 1,2,3,4, …) in each question. A sample
+        screen display when the method is called is given below:
+        How much is 6 times 7? 42
+        How much is 2 times 9? 18
+        How much is 9 times 4? 36
+
+        4 answers out of 5 are correct.
+        The input which is underlined is the student’s answer to a question
+        */
+    
+    }
+
+    public static void divide(int m, int n){
+    /*
+    Write the method divide() which does division by subtraction and returns the quotient of dividing m by
+    n. Both m and n are positive integers (i.e. 1,23,4,…). Division by subtraction means that the division
+    operation is achieved using the subtraction method. For example, divide(12,4) will be performed as
+    follows: 12-4=8, 8-4=4, and then 4-4=0, and it ends and returns the result of 3 as it performs three
+    times in the subtraction operation. No error checking on the parameters is required in the method. 
+    */
+    }
+
+    public static int modulus(int m, int n){
+    /*
+    Write the method modulus() which does division by subtraction and returns the remainder of dividing
+    m by n. Both m and n are positive integers. No error checking on the parameters is required in the
+    method.
+    */
+    }
+
+    public static int countDigits(int n){
+    /*
+    Write a method to count the number of digits for a positive integer (i.e. 1,2,3,4,…). For example, 1234
+    has 4 digits. The method countDigits() returns the result.
+    */
+    }
+
+    public static int position(int n, int digit) {
+    /*
+    Write the method position() which returns the position of the first appearance of a specified digit in a
+    positive number n. The position of the digit is counted from the right and starts from 1. If the required
+    digit is not in the number, the method should return -1. For example, position(12315, 1) returns 2 and
+    position(12, 3) returns -1. No error checking on the parameters is required in the method. 
+    */
+    }
+
+
+
+    public static long extractOddDigits(long n){
+    /*
+    Write a method extractOddDigits() which extracts the odd digits from a positive number n, and
+    combines the odd digits sequentially into a new number. The new number is returned back to the
+    calling method. If the input number n does not contain any odd digits, then returns -1. For examples, if
+    n=1234567, then 1357 is returned; and if n=28, then –1 is returned.
+    */
+    }
 
 }
