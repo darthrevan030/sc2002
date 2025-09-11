c6a6c7af89c9108a08605f43763a2c2ca60343cb
darth.revan.030 <smartsamarth03@gmail.com>
2025-09-11 10:57:49 +0800

    Lab 2 P 4 Not Done

---
+++ Labs/Lab 2/Lab2p4.java
@@ -0,0 +1,12 @@
+/*
+Write the method modulus() which does division by subtraction and returns the remainder of dividing
+m by n. Both m and n are positive integers. No error checking on the parameters is required in the
+method.
+ */
+
+public class Lab2p4 {
+    
+    public static int modulus(int m, int n){
+        
+    }
+}
