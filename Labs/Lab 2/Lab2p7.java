7393f146e49db5b1ff3fad20446474fdd9b6abb4
darth.revan.030 <smartsamarth03@gmail.com>
2025-09-11 10:58:11 +0800

    Lab 2 P 7 Not Done

---
+++ Labs/Lab 2/Lab2p7.java
@@ -0,0 +1,10 @@
+/*
+Write a method extractOddDigits() which extracts the odd digits from a positive number n, and
+combines the odd digits sequentially into a new number. The new number is returned back to the
+calling method. If the input number n does not contain any odd digits, then returns -1. For examples, if
+n=1234567, then 1357 is returned; and if n=28, then –1 is returned.
+*/
+public class Lab2p7{
+    public static long extractOddDigits(long n){
+    }
+}
