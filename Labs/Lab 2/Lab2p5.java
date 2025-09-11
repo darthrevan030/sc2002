68fce2b0a7b55c67fb48cac6cb885e60b841f85d
darth.revan.030 <smartsamarth03@gmail.com>
2025-09-11 10:57:57 +0800

    Lab 2 P 5 Not Done

---
+++ Labs/Lab 2/Lab2p5.java
@@ -0,0 +1,9 @@
+/*
+Write a method to count the number of digits for a positive integer (i.e. 1,2,3,4,…). For example, 1234
+has 4 digits. The method countDigits() returns the result.
+ */
+public class Lab2p5 {
+    public static int countDigits(int n){
+
+    }
+}
