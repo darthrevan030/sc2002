c0f623a61ac37e74a6fa3dc24d2643666c362519
darth.revan.030 <smartsamarth03@gmail.com>
2025-09-11 10:57:24 +0800

    Lab 2 P 2 not done

---
+++ Labs/Lab 2/Lab2p2.java
@@ -0,0 +1,25 @@
+/*
+Write a method that is to test students ability to do multiplication. The method will ask a student 5
+multiplication questions one by one and checks the answers. The method prints out the number of
+correct answers given by the student. The method random() from the Math class of the Java library
+can be used to produce two positive one-digit integers (i.e. 1,2,3,4, …) in each question. A sample
+screen display when the method is called is given below:
+How much is 6 times 7? 42
+How much is 2 times 9? 18
+How much is 9 times 4? 36
+
+4 answers out of 5 are correct.
+The input which is underlined is the student’s answer to a question
+*/
+
+
+import java.util.Scanner;
+import java.lang.Math 
+
+
+public class Lab2p2 {
+    public static void mulTest(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+    }
+}
